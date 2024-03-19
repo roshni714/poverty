ef60f754afd356f62bc93b4c025167c7eaa49700
roshni714 <roshni714@github.com>
2024-03-19 08:46:04 -0700

    replication code

--- replication/generate_sbatches.py
+++ replication/generate_sbatches.py
@@ -16,73 +16,52 @@ SBATCH_PREFACE = """#!/bin/bash
 # constants for commands
 
 OUTPUT_PATH = "/home/users/rsahoo/poverty/replication/scripts/"
-SAVE_PATH = "/home/users/rsahoo/poverty/replication/"
-
-
-def generate_sim_runs():
-    budgets = [0.05, 0.075, 0.10, 0.15, 0.20]
-    #    ds = [2]
-    for budget in budgets:
-        exp_id = "sim_budget={}".format(budget)
-        script_fn = os.path.join(OUTPUT_PATH, "{}.sh".format(exp_id))
-        with open(script_fn, "w") as f:
-            print(
-                SBATCH_PREFACE.format(exp_id, OUTPUT_PATH, exp_id, OUTPUT_PATH, exp_id),
-                file=f,
-            )
-            base_cmd = "python main.py main --d 10 --density_est_method glm --budget {}".format(
-                budget
-            )
-            print(base_cmd, file=f)
-            print("sleep 1", file=f)
-
-
-def generate_uganda_runs():
-    #    ds = [5, 8, 12]
-    #    budgets= [0.05, 0.075, 0.10, 0.15, 0.20]
-
-    ds = [5]
-    budgets = [0.05]
-
-    for d in ds:
-        for budget in budgets:
-            exp_id = "uganda_d={}_budget={}".format(d, budget)
-            script_fn = os.path.join(OUTPUT_PATH, "{}.sh".format(exp_id))
-
-            with open(script_fn, "w") as f:
-                print(
-                    SBATCH_PREFACE.format(
-                        exp_id, OUTPUT_PATH, exp_id, OUTPUT_PATH, exp_id
-                    ),
-                    file=f,
-                )
-                base_cmd = "python main_real.py main --d {} --density_est_method glm --country uganda --budget {}".format(
-                    d, budget
-                )
-                print(base_cmd, file=f)
-                print("sleep 1", file=f)
+SAVE_PATH = "/home/users/rsahoo/poverty/replication/results/"
 
 
 def generate_malawi_runs():
-    ds = [0, 3, 12, 20]
-    budgets = [0.05]
+    ds = [0, 3, 12, 22]
+    budgets = [0.05, 0.075, 0.10, 0.15, 0.20]
+
+    constraints = ["unconditional", "conditional"]
+    quantile_methods = ["qr", "density"]
 
     for d in ds:
-        for budget in budgets:
-            exp_id = "malawi_d={}_budget={}".format(d, budget)
-            script_fn = os.path.join(OUTPUT_PATH, "{}.sh".format(exp_id))
-            with open(script_fn, "w") as f:
-                print(
-                    SBATCH_PREFACE.format(
-                        exp_id, OUTPUT_PATH, exp_id, OUTPUT_PATH, exp_id
-                    ),
-                    file=f,
-                )
-                base_cmd = "python main_real.py main --d {} --density_est_method glm --country malawi --budget {}".format(
-                    d, budget
-                )
-                print(base_cmd, file=f)
-                print("sleep 1", file=f)
+        for constraint in constraints:
+            if constraint == "conditional":
+                for quantile_method in quantile_methods:
+                    exp_id = "malawi_d={}_constraint={}_method={}".format(
+                        d, constraint, quantile_method
+                    )
+                    script_fn = os.path.join(OUTPUT_PATH, "{}.sh".format(exp_id))
+                    with open(script_fn, "w") as f:
+                        print(
+                            SBATCH_PREFACE.format(
+                                exp_id, OUTPUT_PATH, exp_id, OUTPUT_PATH, exp_id
+                            ),
+                            file=f,
+                        )
+                        base_cmd = "python main.py main --d {} --constraint {} --method {} --country malawi --tolerance 0.05 0.075 0.10 0.15 0.20 --save {}".format(
+                            d, constraint, quantile_method, SAVE_PATH
+                        )
+                        print(base_cmd, file=f)
+                        print("sleep 1", file=f)
+
+            elif constraint == "unconditional":
+                exp_id = "malawi_d={}_constraint={}".format(d, constraint)
+                script_fn = os.path.join(OUTPUT_PATH, "{}.sh".format(exp_id))
+                with open(script_fn, "w") as f:
+                    print(
+                        SBATCH_PREFACE.format(
+                            exp_id, OUTPUT_PATH, exp_id, OUTPUT_PATH, exp_id
+                        ),
+                        file=f,
+                    )
+                    base_cmd = "python main.py main --d {} --constraint {} --country malawi --tolerance 0.05 0.075 0.10 0.15 0.20 --save {}".format(
+                        d, constraint, SAVE_PATH
+                    )
+                    print(base_cmd, file=f)
+                    print("sleep 1", file=f)
 
 
 # generate_sim_runs()
--- replication/main.py
+++ replication/main.py
@@ -1,51 +1,73 @@
-from opt_targeted_transfers import ConditionalTargetedTransfers, UnconditionalTargetedTransfers
+from opt_targeted_transfers import (
+    ConditionalTargetedTransfers,
+    UnconditionalTargetedTransfers,
+)
 from data_loaders import get_dataset
 from data_utils import split_data
+from reporting import write_result
 import argh
+
 
 @argh.arg("--tolerance", nargs="+", type=float)
 @argh.arg("--d", default=2)
 @argh.arg("--country", default="malawi")
-@argh.arg("--constraint",  default="uncondtional")
-@argh.arg("--quantile_method", default="qr") # refers to quantile method if constraint = conditional
-@argh.arg("--save", default="malawi_results.csv")
-def main(country="malawi", d=2, tolerance=None, constraint="unconditional", quantile_method = "qr"):
+@argh.arg("--constraint", default="unconditional")
+@argh.arg(
+    "--method", default="qr"
+)  # refers to quantile method if constraint = conditional
+@argh.arg("--save", default="results")
+def main(
+    country="malawi",
+    d=2,
+    constraint="unconditional",
+    method="qr",
+    tolerance=None,
+    save="malawi_results.csv",
+):
     X, y, r, features = get_dataset(country)
 
-    (X_train, y_train, r_train), (X_test, y_test, r_test) = split_data(X=X[:, :d], y=y, r=r, p=0.6)
+    (X_train, y_train, r_train), (X_test, y_test, r_test) = split_data(
+        X=X[:, :d], y=y, r=r, p=0.6
+    )
 
-    fit_first = False
-
-    if constraint == "unconditional":   
-        tt = UnconditionalTargetedTransfer(name=country, c_bar=2.15)
-        fit_first = True
+    if constraint == "unconditional":
+        tt = UnconditionalTargetedTransfers(name=country, c_bar=2.15)
 
     elif constraint == "conditional":
-        tt = ConditionalTargetedTransfer(name=country, quantile_method=quantile_method, c_bar=2.15)
+        tt = ConditionalTargetedTransfers(name=country, method=method, c_bar=2.15)
 
-        if method == "density"
-            fit_first = True
+    fit_first = True
+    if constraint == "conditional" and method == "qr":
+        fit_first = False
 
     if fit_first:
+        print("fitting first")
         tt.fit(X_train, y_train, r_train)
 
         for tol in tolerance:
             tt.set_tolerance(tol)
-            tt.run_opt(X_test, r_test)
+            if constraint == "unconditional":
+                tt.run_opt(
+                    X_test,
+                    r_test,
+                    path=save + "{}_d={}_tol={}_opt.csv".format(country, d, tol),
+                )
+            else:
+                tt.run_opt(X_test, r_test)
             res = tt.evaluate(X_test, y_test, r_test)
-            write_results(res, save)
-
+            write_result(save + "{}.csv".format(country), res)
 
     else:
+        print("setting tolerance first")
         for tol in tolerance:
             tt.set_tolerance(tol)
             tt.fit(X_train, y_train, r_train)
             tt.run_opt(X_test, r_test)
             res = tt.evaluate(X_test, y_test, r_test)
-            write_results(res, save)
+            write_result(save + "{}.csv".format(country), res)
 
 
- 
-
-    
-    
+if __name__ == "__main__":
+    _parser = argh.ArghParser()
+    _parser.add_commands([main])
+    _parser.dispatch()
