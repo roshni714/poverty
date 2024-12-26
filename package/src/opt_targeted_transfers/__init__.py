2644ff921fa58958dff46a2c9564c08eb95c7ac5
Roshni Sahoo <roshni714@github.com>
2024-12-26 14:55:42 -0800

    density estimation hparam search

--- package/src/opt_targeted_transfers/__init__.py
+++ package/src/opt_targeted_transfers/__init__.py
@@ -2,8 +2,6 @@ from opt_targeted_transfers.opt import (
     TargetedTransfers,
     UnconditionalTargetedTransfers,
     ConditionalTargetedTransfers,
-    # UnconditionalDiscreteTransfers,
-    HybridTargetedTransfers,
     BinaryTargetedTransfers,
     OraclePovertyRateTargetedTransfers,
     GapTargetedTransfers,
