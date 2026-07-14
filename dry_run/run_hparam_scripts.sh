dfa08c6e63927f4298f1e96bd72d12a81aaa2c8d
Roshni Sahoo <roshni714@github.com>
2026-07-14 16:40:32 -0700

    scripts

---
+++ dry_run/run_hparam_scripts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+OUTPUT_DIR=$1
+HPARAM_DIR="$OUTPUT_DIR/hparam"
+USE_SBATCH=$2
+mkdir -p $OUTPUT_DIR
+python generate_sbatches.py --output-path $OUTPUT_DIR
+
+for experiment in $HPARAM_DIR/*.sh
+do
+    echo $experiment
+    chmod u+x $experiment
+    if [ "$USE_SBATCH" = true ]; then
+        sbatch $experiment
+    else
+        chmod u+x $experiment
+        $experiment
+    fi
+    sleep 1
+done
+
+echo "Done"
